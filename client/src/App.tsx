c6acc4a6c77978177fbdef7f7af021d7ce0243ca
eNKay <nguyenkhai24082408@gmail.com>
2025-12-26 16:48:47 +0700

    feat(frontend): add Waiter Dashboard and Kitchen Display System (KDS)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import About from './pages/About';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import WaiterOrders from "./pages/waiter/Orders";
+import KDS from "./pages/kitchen/KDS";
 
 function App() {
-    return (
-        <div>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </div>
-    );
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/menu" element={<Menu />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/waiter/orders" element={<WaiterOrders />} />
+				<Route path="/kitchen/kds" element={<KDS />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
-
